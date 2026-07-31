b691a3b5a5e6ca2de4dbf0bc724aec84856b277b
Nemchuk-Egor <nemchuk1994@gmail.com>
2026-07-31 22:59:24 +0300

    Практикуемся в написании API автотестов. Часть 3. Практика написания API автотестов для работы с заданиями. Получение задания

--- tests/exercises/test_exercises.py
+++ tests/exercises/test_exercises.py
@@ -1,4 +1,5 @@
 from http import HTTPStatus
+from urllib import request
 
 import pytest
 
@@ -6,16 +7,24 @@ from clients.exercises.exercises_client import ExercisesClient
 from clients.exercises.exercises_schema import (
     CreateExerciseRequestSchema,
     CreateExerciseResponseSchema,
+    GetExerciseResponseSchema,
 )
 from fixtures.courses import CourseFixture
+from fixtures.exercises import ExerciseFixture
 from tools.assertions.base import assert_status_code
-from tools.assertions.exercise import assert_create_exercise_response
+from tools.assertions.exercise import (
+    assert_create_exercise_response,
+    assert_get_exercise_response,
+)
 from tools.assertions.schema import validate_json_schema
+
 
 @pytest.mark.exercises
 @pytest.mark.regression
 class TestExercises:
-    def test_create_exercise(self, exercises_client: ExercisesClient, function_course: CourseFixture):
+    def test_create_exercise(
+        self, exercises_client: ExercisesClient, function_course: CourseFixture
+    ):
         """
         Проверяет успешное создание задания через API.
         - Отправляется POST-запрос с course_id.
@@ -24,11 +33,36 @@ class TestExercises:
         - Валидируется JSON-схема ответа.
         """
 
-        request = CreateExerciseRequestSchema(course_id=function_course.response.course.id)
+        request = CreateExerciseRequestSchema(
+            course_id=function_course.response.course.id
+        )
         response = exercises_client.create_exercise_api(request)
         response_data = CreateExerciseResponseSchema.model_validate_json(response.text)
 
         assert_status_code(response.status_code, HTTPStatus.OK)
         assert_create_exercise_response(response_data, request)
 
+        validate_json_schema(response.json(), response_data.model_json_schema())
+
+    def test_get_exercise(
+        self,
+        exercises_client: ExercisesClient,
+        function_exercise: ExerciseFixture,
+    ):
+        """
+        Проверяет успешное получение задания по ID.
+        - Создаётся задание через фикстуру.
+        - Выполняется GET-запрос с его ID.
+        - Проверяется статус-код 200.
+        - Сравнивается полученное задание с созданным.
+        - Валидируется JSON-схема ответа.
+        """
+
+        exercise_id = function_exercise.response.exercise.id
+        response_exercise = function_exercise.response
+        response = exercises_client.get_exercise_api(exercise_id)
+        response_data = GetExerciseResponseSchema.model_validate_json(response.text)
+
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        assert_get_exercise_response(response_data, response_exercise)
         validate_json_schema(response.json(), response_data.model_json_schema())
--- tools/assertions/exercise.py
+++ tools/assertions/exercise.py
@@ -1,7 +1,15 @@
-from clients.exercises.exercises_schema import CreateExerciseResponseSchema, CreateExerciseRequestSchema
+from clients.exercises.exercises_schema import (
+    CreateExerciseResponseSchema,
+    CreateExerciseRequestSchema,
+    Exercise,
+    GetExerciseResponseSchema,
+)
 from tools.assertions.base import assert_equal
 
-def assert_create_exercise_response(actual: CreateExerciseResponseSchema, expected: CreateExerciseRequestSchema):
+
+def assert_create_exercise_response(
+    actual: CreateExerciseResponseSchema, expected: CreateExerciseRequestSchema
+):
     """
     Проверяет, что ответ на создание задания соответствует запросу.
     Сравниваются все поля: заголовок, идентификатор курса, баллы, описание, порядковый индекс, оценочное время.
@@ -13,5 +21,23 @@ def assert_create_exercise_response(actual: CreateExerciseResponseSchema, expect
     assert_equal(actual.exercise.min_score, expected.min_score, "min_score")
     assert_equal(actual.exercise.description, expected.description, "description")
     assert_equal(actual.exercise.order_index, expected.order_index, "order_index")
-    assert_equal(actual.exercise.estimated_time, expected.estimated_time, "estimated_time")
-    
+    assert_equal(
+        actual.exercise.estimated_time, expected.estimated_time, "estimated_time"
+    )
+
+
+def assert_exercise(actual: Exercise, expected: Exercise):
+    assert_equal(actual.id, expected.id, "id")
+    assert_equal(actual.title, expected.title, "title")
+    assert_equal(actual.course_id, expected.course_id, "course_id")
+    assert_equal(actual.max_score, expected.max_score, "max_score")
+    assert_equal(actual.min_score, expected.min_score, "min_score")
+    assert_equal(actual.description, expected.description, "description")
+    assert_equal(actual.order_index, expected.order_index, "order_index")
+    assert_equal(actual.estimated_time, expected.estimated_time, "estimated_time")
+
+
+def assert_get_exercise_response(
+    actual: GetExerciseResponseSchema, expected: CreateExerciseResponseSchema
+):
+    assert_exercise(actual.exercise, expected.exercise)
